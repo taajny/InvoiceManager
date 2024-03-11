6ae37619588c4281793302447817f38a2bf260c7
Krzysztof <taajny@gmail.com>
2024-03-11 22:18:13 +0100

    Dodaj pliki projektów.

---
+++ Models/Domains/Invoice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceManager.Models.Domains
+{
+    public class Invoice
+    {
+        public Invoice()
+        {
+            InvoicePositions = new Collection<InvoicePosition>();
+        }
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Tytuł")]
+        public string Title { get; set; }
+        [Display(Name = "Wartość")]
+        public decimal Value { get; set; }
+        [Display(Name = "Sposób płatności")]
+        public int MethodOfPaymentId { get; set; }
+        [Display(Name = "Termin płatności")]
+        public DateTime PaymentDate { get; set; }
+        [Display(Name = "Data utworzenia")]
+        public DateTime CreatedDate { get; set; }
+        [Display(Name = "Uwagi")] 
+        public string Comments { get; set; }
+        [Display(Name = "Klient")] 
+        public int ClientId { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        
+        public Client Client { get; set; }
+        public MethodOfPayment MethodOfPayment { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public ICollection<InvoicePosition> InvoicePositions { get; set; }
+    }
+}
---
+++ Models/Domains/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceManager.Models.Domains
+{
+    public class Product
+    {
+        public Product()
+        {
+            InvoicePositions = new Collection<InvoicePosition>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+
+        public ICollection<InvoicePosition> InvoicePositions { get; set; }
+    }
+} 
